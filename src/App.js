f2e0c8642d26a3e6475c5bf5cea1441d1b2dd5ab
Erik <patoerik93@gmail.com>
2021-09-01 18:07:49 +0200

    deleteItems

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Form from './Components/Form';
 import ToDoList from './Components/TodoList';
 
 function App() {
-
+  //States
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+   useEffect(() =>{
+    console.log("valami");
+  }, [todos]);
+
+  //Functions
+  const filterHandler = () =>{
+    switch(status){
+      case 'completed' : 
+      setFilteredTodos(todos.filter(todo => todo.completed === true))
+      break;
+      case 'uncomplited':
+        setFilteredTodos(todos.filter(todo => todo.completed === false))
+        break;
+        default: 
+        setFilteredTodos(todos);
+        break;
+    }
+  }
+
 
   return (
     <div className="App">
      <header>
        <h1>Teendők</h1>
      </header>
-     <Form inputText={ inputText } todos={ todos } setTodos={ setTodos } setInputText={ setInputText }/>
+     <Form inputText={ inputText } todos={ todos } setTodos={ setTodos } setInputText={ setInputText } setStatus={setStatus}/>
      <ToDoList setTodos={ setTodos }todos={ todos }/>
      </div>
   )
--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -23,8 +23,8 @@ const Todo = ({ text, setTodos, todos, todo}) =>{
     
     return(
         <div className="todo">
-            <li className='todo-item'>{text}</li>
-            <button className="completed-btn">
+            <li className={`todo-item ${todo.completed ? "completed" : ""}`}>{text}</li>
+            <button onClick={completeHandler} className="completed-btn">
                 <i className="fas fa-check"></i>
                 </button>
             <button onClick={deleteHandler} className="trash-btn">
